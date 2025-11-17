5cc5df9cc24be9ddc02bdafe7d1e187dedb11fb1
Patricio <patricio.fernandez.florez@gmail.com>
2025-11-17 21:49:57 +0100

    ,

--- init-databases.sql
+++ init-databases.sql
@@ -2,6 +2,9 @@
 CREATE DATABASE IF NOT EXISTS `newsletter`;
 CREATE DATABASE IF NOT EXISTS `uploads`;
 
+-- Crear usuario si no existe
+CREATE USER IF NOT EXISTS 'hostybee_admin'@'%' IDENTIFIED BY 'Solito674';
+
 -- Otorgar permisos al usuario
 GRANT ALL PRIVILEGES ON `newsletter`.* TO 'hostybee_admin'@'%';
 GRANT ALL PRIVILEGES ON `uploads`.* TO 'hostybee_admin'@'%';
