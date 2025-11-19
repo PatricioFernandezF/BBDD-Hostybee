71e1aa3cab50677df721293cd6fbfd681902b3d4
Patricio <patricio.fernandez.florez@gmail.com>
2025-11-19 16:42:47 +0100

    .

--- init-databases.sql
+++ init-databases.sql
@@ -1,10 +1,10 @@
+-- Crear el usuario hostybee_admin con TODOS los permisos desde el inicio
+CREATE USER IF NOT EXISTS 'hostybee_admin'@'%' IDENTIFIED BY 'Solito674';
+GRANT ALL PRIVILEGES ON *.* TO 'hostybee_admin'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
 -- Crear las bases de datos
 CREATE DATABASE IF NOT EXISTS `newsletter`;
 CREATE DATABASE IF NOT EXISTS `uploads`;
 
--- Crear usuario si no existe
-CREATE USER IF NOT EXISTS 'hostybee_admin'@'%' IDENTIFIED BY 'Solito674';
-
--- Otorgar TODOS los permisos al usuario, incluyendo crear bases de datos
-GRANT ALL PRIVILEGES ON *.* TO 'hostybee_admin'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
